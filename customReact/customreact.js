6215136578b3784cebbf2922abeb39635667c175
hrid1 <hridoycse1@gmail.com>
2024-07-05 00:55:04 +0600

    custom react done

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -7,6 +7,15 @@ function customRender(reactElement, container){
 
     container.appendChild(domElement)
     */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.porps)[prop]
+        
+    }
+    container.appendChild(domElement);
 }
 
 
